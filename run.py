834da6b557b2037612888fb6179194b4ec042cfa
Berthille Igiraneza <berthilleigiraneza@outlook.com>
2024-10-04 20:14:11 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+RWAGASORE_HISTORY_MANAGER = Credentials.from_service_account_file('rwagasore-history-manager.json')
+SCOPE_RHM = RWAGASORE_HISTORY_MANAGER.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPE_RHM)
+SHEET = GSPREAD_CLIENT.open ('Rwagasore History Manager')
+
+sheet1 = SHEET.worksheet ('Sheet1')
+data = sheet1.get_all_values()
+print(data)
